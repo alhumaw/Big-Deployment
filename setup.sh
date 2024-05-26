c25736a748677a9246b846d2b901ac0f128cbc00
alhumaw <alhumaw47@gmail.com>
2024-05-25 18:26:24 -0700

    sudo

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ sudo microk8s.kubectl apply -f .
 
 sudo apt install nginx -y
 sudo cp ~/Big-Deployment/proxy.conf /etc/nginx/conf.d/
-NEW_IP=$(microk8s.kubectl get services | grep flask | awk '{print $3}')
+NEW_IP=$(sudo microk8s.kubectl get services | grep flask | awk '{print $3}')
 sudo sed -i "s/10\.152\.183\.93/$NEW_IP/g" /etc/nginx/conf.d/proxy.conf
 sudo apt-get update -y
 sudo apt-get install -y bison build-essential ca-certificates curl dh-autoreconf doxygen flex gawk git iputils-ping libcurl4-gnutls-dev libexpat1-dev libgeoip-dev liblmdb-dev libpcre3-dev libssl-dev libtool libxml2 libxml2-dev libyajl-dev locales lua5.3-dev pkg-config wget zlib1g-dev libgd-dev m4 automake g++ libperl-dev libxml2-dev libxslt1-dev 
