70890f4728ce0f0871f76705aa03d87b543f6eb5
alhumaw <alhumaw47@gmail.com>
2024-05-25 19:10:58 -0700

    rm

--- setup.sh
+++ setup.sh
@@ -45,5 +45,6 @@ sudo cp /opt/ModSecurity/modsecurity.conf-recommended /etc/nginx/modsec/modsecur
 sudo sed -i 's/^SecRuleEngine DetectionOnly/SecRuleEngine On/' /etc/nginx/modsec/modsecurity.conf
 sudo touch /etc/nginx/modsec/main.conf
 sudo sh -c 'echo "Include /etc/nginx/modsec/modsecurity.conf\nInclude /usr/local/modsecurity-crs/crs-setup.conf\nInclude /usr/local/modsecurity-crs/rules/*.conf" > /etc/nginx/modsec/main.conf'
+sudo mv /usr/local/modsecurity-crs/rules/REQUEST-949-BLOCKING-EVALUATION.conf ~/sudo rm /etc/nginx/sites-enabled/default 
 sudo systemctl restart nginx
 
