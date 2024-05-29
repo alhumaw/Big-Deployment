90f3f730c719d8f4b8d28b075f6f8c78a28d7043
alhumaw <alhumaw47@gmail.com>
2024-05-29 11:28:56 -0700

    monitoring

---
+++ Monitoring/Loggin/hourly_capture.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the paths for the log files with timestamps
+timestamp=$(date +\%Y-\%m-\%d-\%H)
+capture_log="/var/log/honeypot/honeypot-$timestamp.log"
+
+# Run tcpdump in the background and capture packets
+sudo tcpdump -i any 'ip and not port 22 and not port 80' -n -tttt > "$capture_log" &
+TCPDUMP_PID=$!
+
+# Sleep for 58 minutes
+sleep 3480
+
+# Kill
+sudo kill $TCPDUMP_PID
+
+```
+`cat /var/log/honeypot/honeypot-$(date +\%Y-\%m-\%d-\%H).log`
